a6f5330ddda5c9306653eef4a35170514dfbc1db
Shorya <shoryatiwari0409@gmail.com>
2025-12-24 01:36:44 +0530

    fix data types and improve output formatting

--- 01-Syntax-Mastery/01_Variables/bio_data.py
+++ 01-Syntax-Mastery/01_Variables/bio_data.py
@@ -1,11 +1,11 @@
 # Bio Data Script
 
 name = "Shorya"
-age = '16 years'
+age = 16
 target_field = "Artificial Intelligence"
 aspiring_researcher = True
 
 print(f"Name: {name}")
-print(f"Age: {age}")
+print(f"Age: {age} years")
 print(f"Target Field: {target_field}")
 print(f"Aspiring Researcher: {aspiring_researcher}")

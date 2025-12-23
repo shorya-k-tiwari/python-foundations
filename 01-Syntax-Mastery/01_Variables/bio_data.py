a7d1b1310b9de1b36d845d80f459fa6f4d98c689
Shorya <shoryatiwari0409@gmail.com>
2025-12-24 01:34:41 +0530

    generalize bio data variables

---
+++ 01-Syntax-Mastery/01_Variables/bio_data.py
@@ -0,0 +1,11 @@
+# Bio Data Script
+
+name = "Shorya"
+age = '16 years'
+target_field = "Artificial Intelligence"
+aspiring_researcher = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Target Field: {target_field}")
+print(f"Aspiring Researcher: {aspiring_researcher}")

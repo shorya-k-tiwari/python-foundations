6bead4e2fbb179acbea5d4de1c9f7ca43e9bbfc7
Shorya <shoryatiwari0409@gmail.com>
2025-12-24 14:37:05 +0530

    add profile stats variables program

---
+++ 01-Syntax-Mastery/01_Variables/profile_stats.py
@@ -0,0 +1,11 @@
+name = "Shorya"
+age = 16
+daily_study_hours = 8.0
+years_to_target = 1
+disciplined = True
+total_study_hours = daily_study_hours*365*years_to_target
+print(f'Name: {name}')
+print(f'Age: {age} years')
+print(f'Daily Study Hours: {daily_study_hours}')
+print(f'Total Study Hours: {total_study_hours}')
+print(f'Disciplined: {disciplined}')

b2f0dc68fdcf83983092fe8fd518f79ae5db47e6
Shorya <shoryatiwari0409@gmail.com>
2025-12-30 15:19:54 +0530

    feat: add digit to word converter using conditional logic

---
+++ 01-Syntax-Mastery/02_Conditionals/digit_to_word_converter.py
@@ -0,0 +1,34 @@
+''' 
+Digit to Word Converter
+'''
+
+# Get a digit input from the user
+n = int(input('Enter a Digit (0-9)'))
+
+# Convert digit to its word representation
+if n == 0:
+    digit = 'Zero'
+elif n == 1:
+    digit = 'One'
+elif n == 2:
+    digit = 'Two'
+elif n == 3:
+    digit = 'Three'
+elif n == 4:
+    digit = 'Four'
+elif n == 5:
+    digit = 'Five'
+elif n == 6:
+    digit = 'Six'
+elif n == 7:
+    digit = 'Seven'
+elif n == 8:
+    digit = 'Eight'
+elif n == 9:
+    digit = 'Nine'
+else: 
+    print('Invalid Digit!')
+    exit()
+
+# Display the result
+print(f'{digit}')

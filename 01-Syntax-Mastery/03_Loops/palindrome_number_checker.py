5de0a4eaede110ec91c856c713326e4929c7278b
Shorya <shoryatiwari0409@gmail.com>
2026-01-01 23:56:52 +0530

    feat: almost done

--- 01-Syntax-Mastery/03_Loops/palindrome_number_checker.py
+++ 01-Syntax-Mastery/03_Loops/palindrome_number_checker.py
@@ -1,5 +1,41 @@
 '''
 Palindrome Number Checker
 '''
+
 #
 n = int(input('Enter a Number: '))
+
+#
+m = 0
+
+#
+if 0 <= n <= 9:
+    answer = 'Palindrome'
+elif n>9:
+    
+    #
+    original = n 
+    
+    #
+    while n!=0:
+        digit = n%10
+        m = m*10 + digit
+        n = n//10
+
+    # 
+    if m == original:
+        answer = 'Palindrome'
+        print(f'{original} ---> {answer}')
+        exit()
+    else:
+        answer = 'Not a Palindrome'
+        print(f'{original} ---> {answer}')
+        exit()
+elif n<0:
+    answer = 'Not a Palindrome'
+else:
+    print('Please enter a Numeric Value!')
+    exit()
+
+#
+print(f'{n} ---> {answer}')

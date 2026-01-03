84d298c95142f63677bdff5334237ec2906227f9
Shorya <shoryatiwari0409@gmail.com>
2026-01-03 15:41:36 +0530

    feat: add user defined multiplication table generator

--- 01-Syntax-Mastery/03_Loops/multiplication_table_generator.py
+++ 01-Syntax-Mastery/03_Loops/multiplication_table_generator.py
@@ -1,3 +1,21 @@
 '''
 Multiplication Table Generator
 '''
+
+# Take the number whose table is to be generated
+number = int(input('Enter a Number: '))
+
+# Take how many multiples the user wants
+count = int(input('Enter the number of multiples to generate: '))
+
+# Validate the count input
+if count < 0:
+    print('Count cannot be negative!')
+    exit()
+
+# Display table header
+print('\n', f'|=== Multiplication Table ===|')
+
+# Generate multiplication table using for loop
+for i in range(1, count + 1):
+    print(f'{number} x {i} = {number * i}')

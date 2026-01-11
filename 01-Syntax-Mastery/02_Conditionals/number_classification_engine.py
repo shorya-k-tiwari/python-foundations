bed1371b89c3b834226148b178a1a7a09952f20f
Shorya <shoryatiwari0409@gmail.com>
2026-01-11 20:31:23 +0530

    program edited

--- 01-Syntax-Mastery/02_Conditionals/number_classification_engine.py
+++ 01-Syntax-Mastery/02_Conditionals/number_classification_engine.py
@@ -1,43 +1,35 @@
-'''
+"""
 Number Classification Engine
 
-Number Classification Engine determines properties of an input number
-'''
+Determines the sign, parity, and divisibility
+properties of an input integer.
+"""
 
-# Input from user
-n=int(input("Enter a number: "))
+n = int(input("Enter a number: "))
 
-# Initializing output header
-output= 'Output'
-
-# Checking sign
-if n>0:
+if n > 0:
     sign = "Positive"
-elif n<0:
+elif n < 0:
     sign = "Negative"
 else:
-    sign = "Neither Positive nor Negative. It is Zero"
+    sign = "Zero"
 
-# Checking parity
-if n%2==0:
+if n % 2 == 0:
     parity = "Even"
 else:
     parity = "Odd"
 
-# Checking divisibility by 3 and 5
-if n%3==0 and n%5==0:
+if n % 3 == 0 and n % 5 == 0:
     divisibility = "Multiple of both 3 and 5"
-elif n%3==0 and n%5!=0:
+elif n % 3 == 0:
     divisibility = "Multiple of 3 only"
-elif n%5==0 and n%3!=0:
+elif n % 5 == 0:
     divisibility = "Multiple of 5 only"
 else:
-    divisibility = "Not a multiple of both 3 or 5"
+    divisibility = "Not a multiple of 3 or 5"
 
-# Output results
-print(f'Input Number: {n}')
-print()
-print(f'{output:=^36}')
-print(f'Sign: {sign}')
-print(f'Parity: {parity}')
-print(f'Divisibility: {divisibility}')
+print(f"\nInput Number: {n}")
+print('|=== Output ===|')
+print(f"Sign        : {sign}")
+print(f"Parity      : {parity}")
+print(f"Divisibility: {divisibility}")

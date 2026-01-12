5a0e4d4f479e7ac6b9ca618ce2a8070deac3036f
Shorya <shoryatiwari0409@gmail.com>
2026-01-12 20:16:44 +0530

    fix: program edited

--- 01-Syntax-Mastery/03_Loops/prime_number_checker.py
+++ 01-Syntax-Mastery/03_Loops/prime_number_checker.py
@@ -1,34 +1,28 @@
-'''
+"""
 Prime Number Checker
 
-A prime number is a positive integer 
-greater than 1 that has no positive divisors other than 1 and itself
-'''
+Determines whether a given integer is prime.
+"""
 
-# Import necessary function 
 from math import sqrt
 
-# Take input from the user
 n = int(input("Enter a positive integer: "))
 
-# Numbers less than 1 are not prime
-if n < 1:
+if n <= 0:
     print("Invalid input! Prime numbers are greater than 1")
     exit()
 elif n == 1:
-    print(f'{n} is Neither Prime Nor Composite')
+    print("1 is neither prime nor composite")
     exit()
-else:
-    prime = True  # Assume prime initially
 
-    # Check divisibility
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            prime = False
-            break
+is_prime = True
 
-# Display the result
-if prime:
+for i in range(2, int(sqrt(n)) + 1):
+    if n % i == 0:
+        is_prime = False
+        break
+
+if is_prime:
     print(f"{n} is a Prime Number")
 else:
     print(f"{n} is a Composite Number")

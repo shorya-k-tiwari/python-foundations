6a2c41b71f8bac4f8c44886960e1eb693aabdc0c
Shorya <shoryatiwari0409@gmail.com>
2026-01-27 19:48:04 +0530

    feat: added fibonacci sequence generator

--- 01-Syntax-Mastery/03_Loops/fibonacci_sequence_generator.py
+++ 01-Syntax-Mastery/03_Loops/fibonacci_sequence_generator.py
@@ -1,3 +1,21 @@
-'''
+"""
 Fibonacci Sequence Generator
-'''
+
+Generates the first n terms of the Fibonacci sequence using iteration
+"""
+
+n = int(input("Enter how many terms: "))
+
+if n <= 0:
+    result = "Invalid input: n must be greater than 0"
+else:
+    previous, current = 0, 1
+    sequence = []
+
+    for i in range(n):
+        sequence.append(str(previous))
+        previous, current = current, previous + current
+
+    result = "Fibonacci Sequence: " + " ".join(sequence)
+
+print(result)

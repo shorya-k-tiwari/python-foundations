d94cdf0904e00e81819b7fca44e7f9227cb1565c
Shorya <shoryatiwari0409@gmail.com>
2026-01-29 15:36:45 +0530

    base

--- 01-Syntax-Mastery/03_Loops/sum_of_digits_calculator.py
+++ 01-Syntax-Mastery/03_Loops/sum_of_digits_calculator.py
@@ -1,3 +1,13 @@
 '''
 Sum of Digits Calculator
 '''
+
+num = int(input('Enter an Integer:'))  
+n = abs(num)         
+sum = 0
+
+while n > 0:
+    sum += n % 10   
+    n //= 10              
+
+print(f'Sum of Digits: {sum}')

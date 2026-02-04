8ed41fe0c681837c9b9a023c5aff129184d5e7f3
Shorya <shoryatiwari0409@gmail.com>
2026-02-04 22:11:25 +0530

    feat: execution counter closer

--- 01-Syntax-Mastery/04_Functions/.py
+++ 01-Syntax-Mastery/04_Functions/.py
@@ -1,1 +1,18 @@
-'''  '''
+"""
+Execution Counter using Closures
+
+Tracks how many times a function is executed
+"""
+
+def make_counter():
+    count = 0
+    def counter():
+        nonlocal count
+        count += 1
+        print(f"Action executed {count} time(s)")
+    return counter
+
+tracker = make_counter()
+tracker()
+tracker()
+tracker()

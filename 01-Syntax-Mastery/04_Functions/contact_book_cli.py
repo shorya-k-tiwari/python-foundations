508dbb67eadba69e0f9e973511df16fbea10696d
Shorya <shoryatiwari0409@gmail.com>
2026-02-19 20:23:24 +0530

    .

--- 01-Syntax-Mastery/04_Functions/contact_book_cli.py
+++ 01-Syntax-Mastery/04_Functions/contact_book_cli.py
@@ -32,3 +32,41 @@ def delete_contact():
     else:
         print("Contact not found.")
 
+def list_contacts():
+    if not contacts:
+        print("No contacts saved.")
+        return
+
+    print("\nAll Contacts:")
+    for name, phone in contacts.items():
+        print(f"{name} -> {phone}")
+
+
+def menu():
+    while True:
+        print("""
+1. Add Contact
+2. Search Contact
+3. Delete Contact
+4. List Contacts
+5. Exit
+""")
+
+        choice = input("Choose option: ").strip()
+
+        if choice == "1":
+            add_contact()
+        elif choice == "2":
+            search_contact()
+        elif choice == "3":
+            delete_contact()
+        elif choice == "4":
+            list_contacts()
+        elif choice == "5":
+            print("Exiting Contact Book.")
+            break
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    menu()

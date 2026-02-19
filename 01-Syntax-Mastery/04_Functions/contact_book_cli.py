da480244da3843041bfa0593a25c50b7fd9f89ef
Shorya <shoryatiwari0409@gmail.com>
2026-02-19 20:21:16 +0530

    .

--- 01-Syntax-Mastery/04_Functions/contact_book_cli.py
+++ 01-Syntax-Mastery/04_Functions/contact_book_cli.py
@@ -1,1 +1,11 @@
-''''''
+"""
+Contact Book CLI
+Simple command-line contact manager using functions and dictionary storage
+"""
+
+contacts = {}
+
+
+def add_contact():
+    name = input("Enter name: ").strip()
+    phone = input("Enter phone number: ").strip()

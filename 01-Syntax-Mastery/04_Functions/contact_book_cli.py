a1531b0e964daf375148c578eb2e5d85565a805f
Shorya <shoryatiwari0409@gmail.com>
2026-02-19 20:22:03 +0530

    .

--- 01-Syntax-Mastery/04_Functions/contact_book_cli.py
+++ 01-Syntax-Mastery/04_Functions/contact_book_cli.py
@@ -9,3 +9,26 @@ contacts = {}
 def add_contact():
     name = input("Enter name: ").strip()
     phone = input("Enter phone number: ").strip()
+    
+    if name in contacts:
+        print("Contact already exists. Updating number.")
+    contacts[name] = phone
+    print("Contact saved.")
+
+
+def search_contact():
+    name = input("Enter name to search: ").strip()
+    if name in contacts:
+        print(f"{name} -> {contacts[name]}")
+    else:
+        print("Contact not found.")
+
+
+def delete_contact():
+    name = input("Enter name to delete: ").strip()
+    if name in contacts:
+        del contacts[name]
+        print("Contact deleted.")
+    else:
+        print("Contact not found.")
+
